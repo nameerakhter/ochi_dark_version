bf8a32363313302927f19b33869a40380e38efa8
nameerakhter <akhtarnameer@gmail.com>
2024-06-30 17:59:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="h-screen w-full bg-zinc-900 text-white">
+        Hello
+      </div>
+    </div>
+  )
+}
+
+export default App
