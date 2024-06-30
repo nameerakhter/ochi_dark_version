55849b46516b708ea3d37b6acd0b90292e5f0c64
nameerakhter <akhtarnameer@gmail.com>
2024-06-30 18:28:51 +0530

    building Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div>
       <div className="h-screen w-full bg-zinc-900 text-white">
-        Hello
+        <Navbar />
+        {/* <landing page />
+        <marquee />
+        <about />
+        <playful />
+        <cards />
+        <cards />
+        <ready to start />
+        <footer /> */}
       </div>
     </div>
   )
